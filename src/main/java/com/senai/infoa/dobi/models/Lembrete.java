73a9c961b9f5f29f6bd8abf394998acd05fab5c0
João Pedro <joaopedromiranda523@gmail.com>
2026-06-17 09:37:26 -0300

    Update Lembrete.java

--- src/main/java/com/senai/infoa/dobi/models/Lembrete.java
+++ src/main/java/com/senai/infoa/dobi/models/Lembrete.java
@@ -19,7 +19,7 @@ public class Lembrete {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id", nullable = false)
+    @Column(name="lembrete_id", nullable = false)
     private Integer id;
 
     @Column(name="titulo", nullable = false)
