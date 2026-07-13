d6ac564da17b495a49ac747df723c05e15564a89
Thombyo <mebmmathias09@gmail.com>
2026-07-13 09:27:26 -0300

    jorge 5.0

--- src/main/java/com/senai/infoa/dobi/models/Atividade.java
+++ src/main/java/com/senai/infoa/dobi/models/Atividade.java
@@ -1,13 +1,15 @@
 package com.senai.infoa.dobi.models;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "atividade")
@@ -25,10 +27,10 @@ public class Atividade {
     private String descricao;
 
     @Column(name = "data_proposta", nullable = false)
-    private String data_proposta;
+    private LocalDate data_proposta;
 
     @Column(name = "data_entrega", nullable = false)
-    private String data_entrega;
+    private LocalDate data_entrega;
 
     @Column(name = "nota", nullable = false)
     private Double nota;
@@ -43,7 +45,7 @@ public class Atividade {
     public Atividade() {
     }
 
-    public Atividade(Integer id, String titulo, String descricao, String data_proposta, String data_entrega,
+    public Atividade(Integer id, String titulo, String descricao, LocalDate data_proposta, LocalDate data_entrega,
             Double nota, Boolean status, Turma turma) {
         this.id = id;
         this.titulo = titulo;
@@ -79,19 +81,19 @@ public class Atividade {
         this.descricao = descricao;
     }
 
-    public String getData_proposta() {
+    public LocalDate getData_proposta() {
         return data_proposta;
     }
 
-    public void setData_proposta(String data_proposta) {
+    public void setData_proposta(LocalDate data_proposta) {
         this.data_proposta = data_proposta;
     }
 
-    public String getData_entrega() {
+    public LocalDate getData_entrega() {
         return data_entrega;
     }
 
-    public void setData_entrega(String data_entrega) {
+    public void setData_entrega(LocalDate data_entrega) {
         this.data_entrega = data_entrega;
     }
 
@@ -118,6 +120,5 @@ public class Atividade {
     public void setTurma(Turma turma) {
         this.turma = turma;
     }
-
     
 }
